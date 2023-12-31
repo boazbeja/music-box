47d253fe2b7798aa79c16992f2b1673f97bc77e1
boazbeja <153323574+boazbeja@users.noreply.github.com>
2023-12-31 14:48:55 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,20 +3,19 @@ var musicBoxAudio = document.getElementById('musicBoxAudio');
 var windingSound = document.getElementById('windingSound');
 
 function toggleMusic() {
+    var musicBoxImg = document.getElementById('musicBoxImage');
+
     if (!isPlaying) {
-        // Play winding sound first
         windingSound.play();
         windingSound.onended = function() {
-            // Only start playing the music box audio after winding sound ends
             musicBoxAudio.play();
-            document.getElementById('musicBoxImage').classList.add('swinging');
+            musicBoxImg.classList.add('enlargingAndRotating');
             isPlaying = true;
         };
     } else {
-        // If music is already playing, stop and reset the music
         musicBoxAudio.pause();
-        musicBoxAudio.currentTime = 0; // Reset music to the start
-        document.getElementById('musicBoxImage').classList.remove('swinging');
+        musicBoxAudio.currentTime = 0;
+        musicBoxImg.classList.remove('enlargingAndRotating');
         isPlaying = false;
     }
 }
