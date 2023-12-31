3a745892148144470856124b51a7c56b474743df
boazbeja <153323574+boazbeja@users.noreply.github.com>
2023-12-31 10:25:28 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
-// Define global variables for audio elements and playback state
+var isPlaying = false;
 var musicBoxAudio = document.getElementById('musicBoxAudio');
 var windingSound = document.getElementById('windingSound');
-var isPlaying = false;
 
-// Function to handle the music playback
 function toggleMusic() {
     if (!isPlaying) {
+        // Play winding sound first
         windingSound.play();
         windingSound.onended = function() {
+            // Only start playing the music box audio after winding sound ends
             musicBoxAudio.play();
             document.getElementById('musicBoxImage').classList.add('swinging');
             isPlaying = true;
         };
     } else {
+        // If music is already playing, stop and reset the music
         musicBoxAudio.pause();
-        musicBoxAudio.currentTime = 0;
+        musicBoxAudio.currentTime = 0; // Reset music to the start
         document.getElementById('musicBoxImage').classList.remove('swinging');
         isPlaying = false;
     }
-}   }
 }
