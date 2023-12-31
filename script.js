da0167c09072643d885c782785df918fc8685368
boazbeja <153323574+boazbeja@users.noreply.github.com>
2023-12-31 10:09:35 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,23 @@
-document.addEventListener('DOMContentLoaded', 
- function toggleMusic() {
-    var windingSound = document.getElementById('windingSound');
-    var musicBoxAudio = document.getElementById('musicBoxAudio');
+// Define global variables for audio elements and playback state
+var musicBoxAudio = document.getElementById('musicBoxAudio');
+var windingSound = document.getElementById('windingSound');
+var isPlaying = false;
 
+// Function to handle the music playback
+function toggleMusic() {
     if (!isPlaying) {
         // Play winding sound first
         windingSound.play();
-        
-        // Once winding sound ends, play the music
+
+        // When winding sound ends, play the music box audio
         windingSound.onended = function() {
             musicBoxAudio.play();
             isPlaying = true;
         };
     } else {
-        // Pause the music
+        // If music is already playing, stop and reset the music
         musicBoxAudio.pause();
+        musicBoxAudio.currentTime = 0; // Reset music to the start
         isPlaying = false;
-    } 
-     
+    }
+}
